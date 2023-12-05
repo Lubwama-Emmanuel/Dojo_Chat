d8678de5564f65c5f8925efba2bc599e0335812d
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-12-05 22:13:08 +0300

    feat:firebase sdk:

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp, getApp } from "firebase/app";
+import { initializeFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import Constants from "expo-constants";
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.apiKey,
+  //   authDomain: "dojochat-37057.appspot.com",
+  projectId: "dojochat-37057",
+  storageBucket: "dojochat-37057.appspot.com",
+  //   messagingSenderId: "your_messaging_sender_id",
+  appId: "1:922429953234:ios:5de34ccec82709f5a6890b",
+  //   measurementId: "your_measurement_id", // optional
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = initializeFirestore(app, { experimentalForceLongPolling: true });
+
+export { db, auth };
