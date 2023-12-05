513aa4e481560ea0d6c4ac03a56b0f0f45d41b3c
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-12-05 22:17:17 +0300

    feat:Login screen

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,11 +1,47 @@
+import { useState } from "react";
 import { View, StyleSheet, Text, Image, Pressable } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { useNavigation } from "@react-navigation/native";
+
+import { logInUser } from "../utils/api";
 import Button from "../ui/Button";
 import Input from "../ui/Input";
 
-export default function LogIn() {
-  function LogInWithGoogle() {
-    console.log("Sign In With Google");
+export type RootStackParamList = {
+  SignUp: undefined;
+};
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+export default function LogIn(this: any) {
+  const [inputValues, setInputValues] = useState(initialValues);
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+
+  async function authenicateUser() {
+    try {
+      const res = await logInUser(inputValues.email, inputValues.password);
+    } catch (error) {
+      console.log("an error occured here", error);
+      return;
+    }
   }
+
+  function SignUpInstead() {
+    navigation.navigate("SignUp");
+  }
+
+  function handleInputValues(inputIdentifier: string, enteredValue: string) {
+    setInputValues((currentValues) => {
+      return {
+        ...currentValues,
+        [inputIdentifier]: enteredValue,
+      };
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Image source={require("../assets/bg.png")} style={styles.image} />
@@ -14,22 +50,24 @@ export default function LogIn() {
         <Input
           inputConfig={{
             placeholder: "Enter Email",
+            onChangeText: handleInputValues.bind(this, "email"),
           }}
         />
         <Input
           inputConfig={{
             placeholder: "Enter Password",
+            onChangeText: handleInputValues.bind(this, "password"),
           }}
         />
-        <Button background onPress={() => LogInWithGoogle()}>
+        <Button background onPress={authenicateUser}>
           Log In
         </Button>
         <Text>Or</Text>
-        <Button onPress={() => LogInWithGoogle()}>Log In with Google</Button>
+        <Button>Log In with Google</Button>
         <Button>Log In with Apple</Button>
         <Button>Log In with Facebook</Button>
-        <Pressable >
-          <Text>New Here ? Sign Up instead</Text>
+        <Pressable onPress={() => SignUpInstead()}>
+          <Text style={styles.link}>New Here ? Sign Up instead</Text>
         </Pressable>
       </View>
     </View>
@@ -47,7 +85,7 @@ const styles = StyleSheet.create({
     tranform: [{ translateY: 60 }],
     position: "absolute",
     bottom: 0,
-    height: "75%",
+    height: "85%",
     width: "100%",
     backgroundColor: "#fff",
   },
@@ -62,8 +100,12 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     position: "absolute",
-    height: "30%",
+    height: "20%",
     top: 0,
-    resizeMode: "cover",
+  },
+  link: {
+    color: "#92400e",
+    fontSize: 18,
+    marginVertical: 20,
   },
 });
