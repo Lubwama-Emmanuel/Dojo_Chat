82bce9e65c29985f10512d92fcf7ad0a033247a0
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-12-06 19:19:32 +0300

    feat:added gifted chat

--- screens/Chat.tsx
+++ screens/Chat.tsx
@@ -18,6 +18,26 @@ export default function Chat() {
           avatar: "https://placeimg.com/140/140/any",
         },
       },
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
     ]);
   }, []);
 
